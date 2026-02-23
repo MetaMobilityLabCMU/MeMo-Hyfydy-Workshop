fe9da7f2cc77224b94fc0fb27c810b1a926e2904
Nate <68032073+Nate-GitHub@users.noreply.github.com>
2026-02-23 11:33:13 -0500

    writing some notes from the training session for myself

--- model_breakpoints.py
+++ model_breakpoints.py
@@ -1,8 +1,16 @@
+# Often, we want to see what a musculoskeletal model consists of (i.e. get a list of
+# the muscles, bodies, their properties, etc.). We can load scone and manually browse
+# through it all, but if we just want to do so while we're running and debugging code,
+# we need some setup. This file takes just the gym environment, runs it in a single
+# process, and lets us add breakpoints where we want them so that we can inspect the
+# model
+
 import gym
 import env
 
 # create the sconegym env
-env = gym.make("H0918-v0")
+env = gym.make("H0918-v0") # refers us to env/__init__.py which refers to scone_env.py
+# Should be able to add a breakpoint any time after this to inspect self.model
 action = env.action_space.sample()
 for ep in range(100):
     if ep % 10 == 0:
